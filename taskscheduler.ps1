7341eab19f6a726076cf9024f05b6da1dd1cdd0d
mrnobody59420-hub <mr.nobody59420@gmail.com>
2025-11-19 12:40:57 -0500

    Delete taskscheduler.ps1

--- taskscheduler.ps1
+++
@@ -1,8 +0,0 @@
-$action = New-ScheduledTaskAction -Execute powershell.exe -Argument "-ExecutionPolicy Bypass -File C:\Scripts\MyScript.ps1"
-$principal = New-ScheduledTaskPrincipal -UserId NT AUTHORITY\SYSTEM -LogonType ServiceAccount -RunLevel Highest
-
-# Create the trigger with repetition settings
-$trigger = New-ScheduledTaskTrigger -AtStartup -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)
-
-Register-ScheduledTask -Telemetry_PROG -Action $action -Principal $principal -Trigger $trigger -Description "kyle was here"
-Remove-Item -Path "C:\Path\To\Your\File.txt" -Force
